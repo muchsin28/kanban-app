2e903c608e3a8de8e7a05d3e4168f140631e0da0
muchsin28 <hab@muchsin.id>
2020-12-06 20:01:41 +0700

    deploy ulang

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { Authentication, Authorization } = require('../middlewares')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
-// router.post('/google-login', UserController.googleLogin)
+router.post('/google-login', UserController.googleLogin)
 // router.post('/', UserController.githubLogin)
 
 //Authentication Required
@@ -15,13 +15,11 @@ router.post('/tasks', TaskController.create)
 router.get('/tasks', TaskController.getAll)
 router.get('/tasks/:id', TaskController.getById);
 
-router.get('/departments', DepartmentController.getAll)
-
 //Authorization Required
 router.use('/tasks/:id', Authorization);
-// router.pacth('/tasks/:id', TaskController.update)
+router.patch('/tasks/:id', TaskController.update)
 // router.put('/tasks/:id', TaskController.edit)
-// router.delete('/tasks/:id', TaskController.delete)
+router.delete('/tasks/:id', TaskController.delete)
 
 
 module.exports = router
